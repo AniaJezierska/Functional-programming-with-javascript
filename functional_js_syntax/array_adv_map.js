13454c4beb80c45a83375d714b45ffe570fab2db
Ania <candyrockstore5@gmail.com>
2021-07-22 14:53:15 +0200

    Advanced Map Practice

--- functional_js_syntax/array_adv_map.js
+++ functional_js_syntax/array_adv_map.js
@@ -1,20 +1,25 @@
-// 1. TODO
-["1", "2", "3"].map(parseInt)
-
+const { race } = require("q")[
+  // 1. TODO
+  ("1", "2", "3")
+].map(parseInt);
 
 // ----------------------------------------------------------
 // REAL LIFE EXERCISES
 // ----------------------------------------------------------
-// One common thing we use map for in real life is to reformat objects to have a shape that is 
-// better for our purposes. For instance, we only care about the value and first three characters 
+// One common thing we use map for in real life is to reformat objects to have a shape that is
+// better for our purposes. For instance, we only care about the value and first three characters
 // of the id for the races below. Use map to grab those values and create a new array with them.
 
 var index = [
-    {key: 1, sector: 10, t_score: 18, id: '1236n7e8', value: 'Klingon'},
-    {key: 4, sector: 145, t_score: 12, id: '293847hs8', value: 'Minbari'},
-    {key: 8, sector: 214, t_score: 5, id: '283hy8347', value: 'Cylon'},
-    {key: 3, sector: 8346, t_score: 10, id: 'n9837ks857', value: 'Jawa'},
-]
+  { key: 1, sector: 10, t_score: 18, id: "1236n7e8", value: "Klingon" },
+  { key: 4, sector: 145, t_score: 12, id: "293847hs8", value: "Minbari" },
+  { key: 8, sector: 214, t_score: 5, id: "283hy8347", value: "Cylon" },
+  { key: 3, sector: 8346, t_score: 10, id: "n9837ks857", value: "Jawa" },
+];
+
+const result = index.map((race) => {
+  return { name: race.value, idFirstThree: race.id.substring(0, 3) };
+});
 
 // Expected Output:
 // [
