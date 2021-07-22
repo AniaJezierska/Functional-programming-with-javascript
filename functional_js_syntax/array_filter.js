1589896eca054042977e08d9e42f74d72a304e52
Ania <candyrockstore5@gmail.com>
2021-07-22 15:25:55 +0200

    Write a filter function

--- functional_js_syntax/array_filter.js
+++ functional_js_syntax/array_filter.js
@@ -1,11 +1,17 @@
 // 1. Find all the words with more than 7 characters
-const words = ['tardis', 'grok', 'frak', 'blaster', 'klingon', 'shepherd']
+const words = ["tardis", "grok", "frak", "blaster", "klingon", "shepherd"];
+
+const result1 = words.filter((value) => value.length > 7);
+console.log(result1);
 
 // expected output: Array ['shepherd']
 
 // ----------------------------------------------------------
 // 2. Find all even values
-const words = [12, 13, 14, 15, 16, 17]
+const nums = [12, 13, 14, 15, 16, 17];
+
+const result2 = nums.filter((value) => value % 2 === 0);
+console.log(result2);
 
 // expected output: Array [12, 14, 16]
 
@@ -15,32 +21,35 @@ const words = [12, 13, 14, 15, 16, 17]
 // We often use filter to quickly pull all the items that share a status or other characteristic. For instance, create a list of all the active bounty hunters from the array below:
 
 const hunters = [
-    {
-        name: 'Greedo',
-        universe: 'Star Wars',
-        status: 'active',
-    },
-    {
-        name: 'Boba Fett',
-        universe: 'Star Wars',
-        status: 'inactive',
-    },
-    {
-        name: 'Asajj Ventress',
-        universe: 'Star Wars',
-        status: 'unknown',
-    },
-    {
-        name: 'Zam Wesell',
-        universe: 'Star Wars',
-        status: 'inactive',
-    },
-    {
-        name: 'Jango Fett',
-        universe: 'Star Wars',
-        status: 'active',
-    },
-]
+  {
+    name: "Greedo",
+    universe: "Star Wars",
+    status: "active",
+  },
+  {
+    name: "Boba Fett",
+    universe: "Star Wars",
+    status: "inactive",
+  },
+  {
+    name: "Asajj Ventress",
+    universe: "Star Wars",
+    status: "unknown",
+  },
+  {
+    name: "Zam Wesell",
+    universe: "Star Wars",
+    status: "inactive",
+  },
+  {
+    name: "Jango Fett",
+    universe: "Star Wars",
+    status: "active",
+  },
+];
+
+const result3 = hunters.filter((hunter) => hunter.status === "active");
+console.log(result3);
 
 // expected output: Array [
 //     {
