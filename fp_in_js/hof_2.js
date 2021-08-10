2369bf74a57b6bab9d4c628decad9097f6f99a4d
AniaJezierska <candyrockstore5@gmail.com>
2021-08-10 09:59:50 +0200

    Update hof_2.js

--- fp_in_js/hof_2.js
+++ fp_in_js/hof_2.js
@@ -6,16 +6,6 @@ weaponsWithNoises = [
     {name: 'Noisy Cricket', noise: 'Pew Pew', universe: 'Men in Black'}
 ]
 
-function weaponsFromUniverse(universe) {
-    // ...Your code here!
-}
-
-// USAGE
-const useStarWarsWeapon = weaponsFromUniverse('Star Wars')
-
-useStarWarsWeapon('Blaster') // console logs 'used Blaster: Pew Pew'
-useStarWarsWeapon('Noisy Cricket') // console logs 'Noisy Cricket is not a part of the Star Wars universe'
-
 
 // solution:
 function weaponsFromUniverse(universe) {
