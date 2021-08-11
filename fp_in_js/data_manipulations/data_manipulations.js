07d80dec2a8213c43c89f948a784da227140a180
AniaJezierska <candyrockstore5@gmail.com>
2021-08-11 19:14:22 +0200

    Update data_manipulations.js

--- fp_in_js/data_manipulations/data_manipulations.js
+++ fp_in_js/data_manipulations/data_manipulations.js
@@ -25,7 +25,19 @@ if (i + 1 == arr.length) {
 
 // Hazardous -----------------------------------------------
 // 3. A list of all objects (their id, name, max size in miles, and closest approach in miles) that are labeled potentially hazardous
-
+const hazardous = api_sample_data['near_earth_objects']['2019-12-02'].filter(asteroid => asteroid['is_potentially_hazardous_asteroid'] === true)
+console.log(hazardous[0])
+const hazardousFmt = hazardous.map(asteroid => {
+ const closeApproach = asteroid['close_approach_data']
+ return {
+    id: asteroid['id'],
+    name: asteroid['name'],
+    maxSize: asteroid['estimated_diameter']['miles']['estimated_diameter_max'],
+    closestApproach: closeApproach[0]['miss_distance']['miles']
+ }
+})
+// expected output: 
+// [{"id":"3797749","name":"(2018 BO1)","maxSize":0.2933532873,"closestApproach":"30311929.3487318204"},{"id":"3740494","name":"(2016 AF193)","maxSize":0.1539539936,"closestApproach":"9930345.1795315036"}]
 
 // Too Close for Comfort -----------------------------------
 // 4. A list of all objects (their id, name, max size in miles, and closest approach in miles) that have a miss_distance of less than 900,000 miles
